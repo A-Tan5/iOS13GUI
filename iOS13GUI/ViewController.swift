48cd77ff64e82dc6956fdfbc441e8707679786d9
A-Tan5 <paocd2971@gmail.com>
2020-07-20 15:17:03 +0800

    Initial Commit

---
+++ iOS13GUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS13GUI
+//
+//  Created by tantsunsin on 2020/7/20.
+//  Copyright © 2020 tantsunsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
