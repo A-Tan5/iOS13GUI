f18d590889ec53b5c18fbeb211418538173efc02
A-Tan5 <paocd2971@gmail.com>
2020-07-25 12:34:55 +0800

    sketch & zeplin實作：製作iOS 13的捷徑(shortcut app)畫面

--- iOS13GUI/ViewController.swift
+++ iOS13GUI/ViewController.swift
@@ -12,9 +12,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        setTextField()
         // Do any additional setup after loading the view.
     }
 
-
+//    @IBOutlet weak var SearchTextField: UITextField!
+//
+//    func setTextField(){
+//        SearchTextField.attributedPlaceholder = NSAttributedString(string: "Search", attributes: [NSAttributedString.Key.foregroundColor : UIColor.black])
+//        SearchTextField.placeholderRect(forBounds: CGRect(x: 50, y: 0, width: 0, height: 0))
+//
+//        let view = UIImageView(frame: CGRect(x: 10, y: 0, width: 14, height: 14))
+//        view.image = UIImage(systemName: "magnifyingglass")
+//        view.tintColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.36)
+//
+//        let subleftview = UIView(frame: CGRect(x: 10, y: 11, width: 14, height: 14))
+//        subleftview.addSubview(view)
+//
+//        SearchTextField.leftView = subleftview
+//        SearchTextField.leftViewRect(forBounds: CGRect(x: 50, y: 50, width: 50, height: 50))
+//        SearchTextField.leftViewMode = .always
+//
+//    }
+    
+    
 }
 
